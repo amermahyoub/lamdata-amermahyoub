5201695450ef4adcb95e46ef79aff5abd487281d
ameralhomdy <ameralhomdy@gmail.com>
2019-10-29 13:13:49 -0700

    added comments to code

--- lambdata_dir/__init__.py
+++ lambdata_dir/__init__.py
@@ -1,5 +1,5 @@
 """
-lambdata - a collection od data cience helper functions
+lambdata - a collection of data science helper functions
 """
 import pandas as pd
 import numpy as np
@@ -8,12 +8,21 @@ import datetime
 
 
 def train_val_test(x):
+    """
+    takes a dataframe and splits it into 3 dataframes
+    train set with 60% of df, val set with 20% of df
+    test set with 20% of df
+    """
     train, val = train_test_split(x)
     train, test = train_test_split(train)
     return train, val, test
 
 
 def split_date(df.x):
+    """
+    takes a column from the dataframe and creates new columns
+    for day, month, and year
+    """
     df['day'] = df.x.date.day
     df['month'] = df.x.date.month
     df['year'] = df.x.date.year
